3e4f7f50ca7a4e889a7472b62352a8310c116002
fabiano <fabianoamaral445@gmail.com>
2020-10-23 20:49:19 -0300

    criando metodos para encortar caminho entre cidades

--- esp_estado/espaco_estado.py
+++ esp_estado/espaco_estado.py
@@ -15,6 +15,9 @@ class Espaco_de_Estados(object):
 
     def visinhancas(self):
         return self.monta_visinhanca()
+
+    def visinhancas_com_peso(self):
+        return self.monta_visinhaca_com_peso()
     '''
         Adiciona uma nova cidade aspaço de estados
         A principio ela fica sem cidades visinhas
@@ -37,11 +40,61 @@ class Espaco_de_Estados(object):
 
         borda = []
 
-        for cidade in self.espaco_de_estados:
-            for visinho in self.espaco_de_estados[cidade]:
-                if {visinho, cidade} not in borda:
-                    borda.append({cidade, visinho})
+        for cidade, visinho in self.espaco_de_estados.items():
+            for k in visinho:
+                for c, p in k.items():
+                    if {c, cidade} not in borda:
+                        borda.append({cidade, c})
         return borda
+
+    def monta_visinhaca_com_peso(self):
+        borda = []
+
+        for cidade, visinho in self.espaco_de_estados.items():
+            for k in visinho:
+                for c, p in k.items():
+                    if {c, cidade, p} not in borda:
+                        borda.append({cidade, c, p})
+        return borda
+
+    def encontrar_caminho(self, inicio, fim, caminho=None):
+        if caminho==None:
+            caminho = []
+
+        cidades = self.espaco_de_estados
+        caminho = caminho+[inicio]
+        if inicio == fim:
+            return caminho
+        if inicio not in cidades:
+            return None
+        for cidade in cidades[inicio]:
+            for c in cidade.keys():
+                if c not in caminho:
+                    caminho_extendido = self.encontrar_caminho(c, fim, caminho)
+                    if caminho_extendido:
+                        return caminho_extendido
+        return None
+
+    def encontrar_caminho_com_custo(self, inicio, fim, caminho=None, peso=0):
+        if caminho == None:
+            caminho = []
+        cidades = self.espaco_de_estados
+        caminho = caminho + [inicio]
+        custo = peso
+        if inicio == fim:
+            return caminho
+        if inicio not in cidades:
+            return None
+        for cidade in cidades[inicio]:
+            for c, v in cidade.items():
+                if c not in caminho:
+                    caminho_extendido = self.encontrar_caminho_com_custo(c, fim, caminho, custo+v)
+                    if caminho_extendido:
+                        custo_caminho = {
+                            'Caminho': caminho_extendido,
+                            'Custo': custo
+                        }
+        return None
 
     def __repr__(self):
         return f'{self.espaco_de_estados}'
---
+++ test.py
@@ -0,0 +1,10 @@
+from esp_estado.espaco_estado import Espaco_de_Estados
+from dados.dados import Arquivo
+
+if __name__ == '__main__':
+    grafo = Arquivo.extrair_dados('grafo_8_cidades.txt')
+    esp = Espaco_de_Estados(grafo)
+
+    print(esp.get_cidades())
+    print(esp.visinhancas())
+    print(esp.visinhancas_com_peso())
